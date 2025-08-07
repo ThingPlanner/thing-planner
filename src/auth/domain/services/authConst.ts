e6af1a6d33da37767298758f5577078781ad8453
OliverKeefe <oliverkeefe@tutanota.com>
2025-08-07 23:01:56 +0100

    refactor: switch from Deno.js to Node.js

--- src/auth/domain/services/authConst.ts
+++
@@ -1,7 +0,0 @@
-
-
-export const CLIENT_ID = "thingplanner-api";
-export const CLIENT_SECRET = "{$}";
-export const REDIRECT_URI = "http://localhost:8081/callback";
-export const AUTH_URL = "http://localhost:8080/realms/thingplanner/protocol/openid-connect/auth";
-export const TOKEN_URL = "http://localhost:8080/realms/thingplanner/protocol/openid-connect/token";
