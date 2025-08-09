ad87ad11350f40823d7ddf7dcda06c56c139ad85
OliverKeefe <oliverkeefe@tutanota.com>
2025-08-09 16:11:28 +0100

    chore: update dependencies, add lombok and mapstruct annotationProcessor

--- backend/build.gradle.kts
+++ backend/build.gradle.kts
@@ -21,12 +21,29 @@ extra["springModulithVersion"] = "1.4.1"
 
 dependencies {
     implementation("org.springframework.modulith:spring-modulith-starter-core")
+
     developmentOnly("org.springframework.boot:spring-boot-devtools")
+
     runtimeOnly("org.postgresql:postgresql")
+
     testImplementation("org.springframework.boot:spring-boot-starter-test")
+
     testImplementation("org.springframework.modulith:spring-modulith-starter-test")
+
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+
     implementation("org.springframework.boot:spring-boot-starter-web")
+
+    // https://mvnrepository.com/artifact/org.projectlombok/lombok
+    implementation("org.projectlombok:lombok:1.18.38")
+    annotationProcessor("org.projectlombok:lombok")
+
+    // https://mvnrepository.com/artifact/org.mapstruct/mapstruct
+    implementation("org.mapstruct:mapstruct:1.6.3")
+    annotationProcessor("org.mapstruct:mapstruct-processor:1.6.3")
+
+    // https://mvnrepository.com/artifact/org.springframework.boot/spring-boot-starter-data-jpa
+    implementation("org.springframework.boot:spring-boot-starter-data-jpa:3.5.4")
 }
 
 dependencyManagement {
