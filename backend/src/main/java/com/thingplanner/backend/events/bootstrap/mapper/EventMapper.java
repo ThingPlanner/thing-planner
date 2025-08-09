f7d612c50b38718a529bef2dd6ef4270f15df33b
OliverKeefe <oliverkeefe@tutanota.com>
2025-08-09 16:11:42 +0100

    feat(events): add EventMapper

--- backend/src/main/java/com/thingplanner/backend/events/bootstrap/mapper/EventMapper.java
+++ backend/src/main/java/com/thingplanner/backend/events/bootstrap/mapper/EventMapper.java
@@ -3,10 +3,11 @@ package com.thingplanner.backend.events.bootstrap.mapper;
 import com.thingplanner.backend.events.adapter.api.dto.request.EventRequest;
 import com.thingplanner.backend.events.adapter.data.entity.EventEntity;
 import com.thingplanner.backend.events.core.model.EventModel;
+import org.mapstruct.Mapper;
 import org.springframework.stereotype.Component;
 
 @Mapper(componentModel = "spring")
-public class EventMapper {
+public interface EventMapper {
     EventEntity modelToEntity(EventModel model);
     EventModel entityToModel(EventEntity entity);
     EventModel requestToModel(EventRequest request);
