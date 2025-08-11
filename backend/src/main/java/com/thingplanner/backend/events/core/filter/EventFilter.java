15a16552aa0fbdd88cfa704cd5a95e9328650b2c
OliverKeefe <oliverkeefe@tutanota.com>
2025-08-11 23:44:10 +0100

    fix(events): change EventTypeEntity to EventTypeRequest

--- backend/src/main/java/com/thingplanner/backend/events/core/filter/EventFilter.java
+++ backend/src/main/java/com/thingplanner/backend/events/core/filter/EventFilter.java
@@ -1,13 +1,13 @@
 package com.thingplanner.backend.events.core.filter;
 
-import com.thingplanner.backend.events.adapter.data.entity.EventTypeEntity;
+import com.thingplanner.backend.events.adapter.api.dto.request.EventTypeRequest;
 
 import java.time.ZonedDateTime;
 
 public record EventFilter(
         Long id,
         String name,
-        EventTypeEntity eventType,
+        EventTypeRequest eventType,
         ZonedDateTime startDateTime,
         ZonedDateTime endDateTime
 ) {}
