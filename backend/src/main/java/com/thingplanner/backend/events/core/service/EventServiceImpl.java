fed6837051375cbfe6c3edd04b1fe77b62cbe1f7
OliverKeefe <oliverkeefe@tutanota.com>
2025-08-11 23:42:19 +0100

    feat(events): implement findByFields method

--- backend/src/main/java/com/thingplanner/backend/events/core/service/EventServiceImpl.java
+++ backend/src/main/java/com/thingplanner/backend/events/core/service/EventServiceImpl.java
@@ -2,9 +2,25 @@ package com.thingplanner.backend.events.core.service;
 
 import com.thingplanner.backend.events.adapter.api.dto.request.EventRequest;
 import com.thingplanner.backend.events.adapter.api.dto.response.EventResponse;
+import com.thingplanner.backend.events.bootstrap.mapper.EventMapper;
+import com.thingplanner.backend.events.core.filter.EventFilter;
+import com.thingplanner.backend.events.core.model.EventModel;
 import com.thingplanner.backend.events.core.port.inbound.EventServicePort;
+import com.thingplanner.backend.events.core.port.outbound.EventRepositoryPort;
+
+import java.util.Collections;
+import java.util.Optional;
+import java.util.Set;
 
 public class EventServiceImpl implements EventServicePort {
+
+    private final EventRepositoryPort eventRepositoryPort;
+    private final EventMapper eventMapper;
+
+    public EventServiceImpl(EventRepositoryPort eventRepositoryPort, EventMapper eventMapper ) {
+        this.eventRepositoryPort = eventRepositoryPort;
+        this.eventMapper = eventMapper;
+    }
 
     @Override
     public void create(EventRequest request) {
@@ -12,8 +28,9 @@ public class EventServiceImpl implements EventServicePort {
     }
 
     @Override
-    public EventResponse findByFields(EventRequest request) {
-        return null;
+    public Set<EventModel> findByFields(EventFilter eventFilter) {
+        return eventRepositoryPort.findByFields(eventFilter)
+                .orElse(Collections.emptySet());
     }
 
     @Override
