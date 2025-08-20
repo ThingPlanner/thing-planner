ac3fbbd4f5b482abe6b007f55be93323eba15b72
OliverKeefe <oliverkeefe@tutanota.com>
2025-08-20 21:51:30 +0100

    test(events): add EventRepositoryTest annotations

--- backend/src/test/java/com/thingplanner/backend/mevents/EventRepositoryTest.java
+++ backend/src/test/java/com/thingplanner/backend/mevents/EventRepositoryTest.java
@@ -1,5 +1,11 @@
 package com.thingplanner.backend.mevents;
 
 
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+@ExtendWith(SpringExtension.class)
+@WebMvcTest(EventRepositoryTest.class)
 public class EventRepositoryTest {
 }
