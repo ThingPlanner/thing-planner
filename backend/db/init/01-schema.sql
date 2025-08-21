d52d7d84889e41e7c59ef81120c7cf452c8a9d6d
OliverKeefe <oliverkeefe@tutanota.com>
2025-08-21 20:12:01 +0100

    feat(db): create organizations_events intermediary table

--- backend/db/init/01-schema.sql
+++ backend/db/init/01-schema.sql
@@ -8,3 +8,17 @@ CREATE TABLE IF NOT EXISTS organization (
 );
 
 
+CREATE TABLE IF NOT EXISTS organizations_events (
+    organization_id UUID NOT NULL,
+    event_id UUID NOT NULL,
+    PRIMARY KEY (organization_id, event_id),
+    CONSTRAINT fk_org
+        FOREIGN KEY (organization_id) REFERENCES organization(id)
+        ON DELETE CASCADE ,
+    CONSTRAINT fk_event
+        FOREIGN KEY (event_id) REFERENCES events (id)
+        ON DELETE CASCADE
+);
+
+
+
