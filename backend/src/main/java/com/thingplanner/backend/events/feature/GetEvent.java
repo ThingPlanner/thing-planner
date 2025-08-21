bd2c5d20a880653d3349afea33a4977559274fb5
OliverKeefe <oliverkeefe@tutanota.com>
2025-08-21 22:51:06 +0100

    feat(backend/shared): add GetEventRequest dto

--- backend/src/main/java/com/thingplanner/backend/events/feature/GetEvent.java
+++ backend/src/main/java/com/thingplanner/backend/events/feature/GetEvent.java
@@ -1,6 +1,25 @@
 package com.thingplanner.backend.events.feature;
 
 
+import com.thingplanner.backend.events.model.EventRepository;
+import com.thingplanner.backend.events.model.EventType;
+import com.thingplanner.backend.shared.api.dto.response.MessageResponse;
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+import org.springframework.context.annotation.Bean;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.time.ZonedDateTime;
+import java.util.UUID;
+
+import static com.thingplanner.backend.events.model.EventSpecification.getEventSpec;
+
 @RestController
 @RequestMapping("/events")
 class GetEventApi {
@@ -29,3 +48,13 @@ class GetEventApi {
         }
     }
 }
+
+record GetEventRequest (
+        @NotNull(message = "Event id cannot be null")
+        @Size(min = 36, message = "Event id must be UUID")
+        UUID id,
+        String name,
+        EventType eventType,
+        ZonedDateTime startDateTime,
+        ZonedDateTime endDateTime
+) {};
