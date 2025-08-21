7eed4de8743c3b15d30c3bb460235a513a0e5ca0
OliverKeefe <oliverkeefe@tutanota.com>
2025-08-21 22:50:08 +0100

    feat(backend/shared): add GetEventApi

--- backend/src/main/java/com/thingplanner/backend/events/feature/GetEvent.java
+++ backend/src/main/java/com/thingplanner/backend/events/feature/GetEvent.java
@@ -1,3 +1,31 @@
 package com.thingplanner.backend.events.feature;
 
 
+@RestController
+@RequestMapping("/events")
+class GetEventApi {
+    private final GetEventService getEventService;
+
+
+    public GetEventApi(GetEventService getEventService) {
+        this.getEventService = getEventService;
+    }
+
+    @RequestMapping("/get")
+    public ResponseEntity<?> getEvent(@Valid @RequestBody GetEventRequest request) {
+        try {
+            return getEventService.get(request);
+        } catch (RuntimeException e) {
+            return new MessageResponse("Failure", "Could not get events by specified criteria");
+        }
+    }
+
+    @RequestMapping("/get{id}")
+    public getEventById(@RequestParam UUID id) {
+        try {
+            return getEventService.getById(id);
+        } catch (RuntimeException e) {
+            return new MessageResponse("Failure", String.format("Could not get event with ID: %s", id));
+        }
+    }
+}
