1bd33dd7ce3ace7f47e2bb7e575faabd30bfac67
OliverKeefe <oliverkeefe@tutanota.com>
2025-08-23 16:10:18 +0100

    feat(events): add updateEventSpecification

--- backend/src/main/java/com/thingplanner/backend/events/model/EventSpecification.java
+++ backend/src/main/java/com/thingplanner/backend/events/model/EventSpecification.java
@@ -50,4 +50,43 @@ public class EventSpecification {
             return criteriaBuilder.and(predicates.toArray(new Predicate[0]));
         };
     }
+    public static Specification<Event> updateEventSpec(
+            UUID id,
+            String name,
+            Long eventTypeId,
+            String eventTypeName,
+            ZonedDateTime startDateTime,
+            ZonedDateTime endDateTime
+    ) {
+
+        return (root, criteriaQuery, criteriaBuilder) -> {
+            List<Predicate> predicates = new ArrayList<>();
+
+            if (id != null) {
+                predicates.add(criteriaBuilder.equal(root.get("id"), id));
+            }
+
+            if (name != null && !name.isBlank()) {
+                predicates.add(criteriaBuilder.equal(root.get("name"), name));
+            }
+
+            if (eventTypeId != null) {
+                predicates.add(criteriaBuilder.equal(root.get("eventType").get("id"), eventTypeId));
+            }
+
+            if (eventTypeName != null && !eventTypeName.isBlank()) {
+                predicates.add(criteriaBuilder.equal(root.get("eventType").get("name"), eventTypeName));
+            }
+
+            if (startDateTime != null) {
+                predicates.add(criteriaBuilder.equal(root.get("startDateTime"), startDateTime));
+            }
+
+            if (endDateTime != null) {
+                predicates.add(criteriaBuilder.equal(root.get("endDateTime"), endDateTime));
+            }
+
+            return criteriaBuilder.and(predicates.toArray(new Predicate[0]));
+        };
+    }
 }
