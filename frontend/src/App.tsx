4f4b0ef71f3b460272c51f974d5842d040ee2fb8
OliverKeefe <oliverkeefe@tutanota.com>
2025-08-26 20:38:37 +0100

    feat(frontend): add layout and theme provider to App

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,16 @@
 import './App.css'
+import Layout from './components/layout/Layout';
+import CreateEvent from "/home/oliver/Development/ThingPlanner/thing-planner/frontend/src/features/event/components/forms/create-event.tsx";
+import {ThemeProvider} from "@/components/theme/theme-provider.tsx";
 
 function App() {
 
   return (
-    <>
-    </>
+    <ThemeProvider defaultTheme="system" storageKey="vite-ui-theme">
+        <Layout>
+            <p>Hello</p>
+        </Layout>
+    </ThemeProvider>
   )
 }
 
