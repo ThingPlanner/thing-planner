cd156b8d8608df5c7cc62563c7122841bf250df0
OliverKeefe <oliverkeefe@tutanota.com>
2025-08-27 22:26:12 +0100

    feat(frontend/algorithms) add DirectoryPath class

--- frontend/src/utils/algorithms/search/directory-path.ts
+++ frontend/src/utils/algorithms/search/directory-path.ts
@@ -4,4 +4,20 @@ type DirNode = {
     isBottom: boolean
 };
 
+class DirectoryPath {
+    rootNode: DirNode;
+    dirPath: DirNode[];
+    cur: DirNode;
+
+    constructor(rootNode: DirNode) {
+        this.rootNode = rootNode;
+        this.dirPath = [rootNode];
+
+        if (this.dirPath.length > 1) {
+            this.cur = this.dirPath[this.dirPath.length - 1]
+        } else {
+            this.cur = rootNode;
+        }
+    }
+
 
