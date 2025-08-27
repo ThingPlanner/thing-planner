879cb8b978ae116da4890b906d225ae61c497c5c
OliverKeefe <oliverkeefe@tutanota.com>
2025-08-27 22:24:06 +0100

    feat(frontend/ui): add AppTopbar to layout

--- frontend/src/components/layout/Layout.tsx
+++ frontend/src/components/layout/Layout.tsx
@@ -1,12 +1,15 @@
 import {SidebarProvider, SidebarTrigger} from "@/components/ui/sidebar.tsx";
 import {AppSidebar} from "@/components/navigation/app-sidebar.tsx";
+import {AppTopbar} from "@/components/navigation/app-topbar.tsx";
 
 
 export default function Layout({children}: {children: React.ReactNode}) {
     return (
         <SidebarProvider>
             <AppSidebar />
+
             <main className={"flex-1"}>
+                <AppTopbar />
                 <SidebarTrigger />
                 {children}
             </main>
