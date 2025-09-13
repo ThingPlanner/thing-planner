10c5e9608d210f951fb85cce19074c463e0dd322
OliverKeefe <oliverkeefe@tutanota.com>
2025-09-13 16:49:53 +0100

    feat(backend/pages): implement Page model

--- backend/src/main/java/com/thingplanner/features/pages/model/Page.java
+++ backend/src/main/java/com/thingplanner/features/pages/model/Page.java
@@ -1,5 +1,34 @@
 package com.thingplanner.features.pages.model;
 
 
-public class Page {
+import io.quarkus.hibernate.orm.panache.PanacheEntityBase;
+import jakarta.persistence.*;
+
+import java.util.UUID;
+
+@Entity
+@Table (name = "pages")
+public class Page extends PanacheEntityBase {
+
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    public Long id;
+
+    @Column(name = "title")
+    public String title;
+
+    @ManyToOne
+    @Column(name = "thing_id")
+    public UUID thingId;
+
+    @ManyToOne
+    @Column(name = "parent_id")
+    public Long parentId;
+
+    @Column(name = "url")
+    public String url;
+
+    public static Page findById(Long id) {
+        return PanacheEntityBase.find("id", id).firstResult();
+    }
+
 }
