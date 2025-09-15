99b5fe568c7b810f0147dce5dc909ddaddf55110
OliverKeefe <oliverkeefe@tutanota.com>
2025-09-15 21:05:41 +0100

    feat(backend/pages): add getPages endpoint

--- backend/src/main/java/com/thingplanner/features/pages/usecase/GetPage.java
+++ backend/src/main/java/com/thingplanner/features/pages/usecase/GetPage.java
@@ -1,24 +1,38 @@
 package com.thingplanner.features.pages.usecase;
 
+import com.thingplanner.features.pages.model.Page;
 import jakarta.annotation.Resource;
 import jakarta.enterprise.context.ApplicationScoped;
+import jakarta.inject.Inject;
 import jakarta.validation.constraints.NotNull;
 import jakarta.ws.rs.Path;
 import jakarta.ws.rs.PathParam;
 import jakarta.ws.rs.core.Response;
 
+import java.util.List;
 import java.util.UUID;
 
 @Resource
 @Path("/pages")
 class GetPageApi {
 
+    @Inject GetPageService getPageService;
+
     @Path("/get{pageId}")
     public Response getPageById(@PathParam("pageId", UUID pageId)) {}
 
     @Path("/get")
-    public Response getPages(GetPageRequest request) {}
-
+    public Response getPages(GetPageRequest request) {
+        try {
+            List<GetPageResponse> responses = getPageService.getPages(request);
+            return Response.status(200)
+                    .entity(responses)
+                    .build();
+        } catch (Exception e){
+            return Response.status(404)
+                    .entity("Could not find pages.")
+                    .build();
+        }
     }
 }
 
