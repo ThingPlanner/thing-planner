7cd8594c7b9237915c05e425369e59d93e465934
OliverKeefe <oliverkeefe@tutanota.com>
2025-09-16 21:12:48 +0100

    frontend(features/shared): add breadcrumb trail to page component

--- frontend/src/features/shared/pages/page.tsx
+++ frontend/src/features/shared/pages/page.tsx
@@ -1,7 +1,27 @@
-import { useParams, Link } from "react-router-dom";
+import { Container } from "@/components/layout/container/container";
+import {useParams, Link, Route, useNavigate} from "react-router-dom";
+import {BreadcrumbTrail} from "@/components/navigation/breadcrumb-trail.tsx";
+import {useState} from "react";
+import { useBreadcrumb } from "@/features/pages/use-case/get-breadcrumb.ts";
 
-function Page() {
-    const { pageId } = useParams<{ pageId: string }>();
-
+export function Page() {
     const [blocks, setBlocks] = useState([]);
+    const { pageId } = useParams<{ pageId: string }>();
+    const { breadcrumb, loading } = useBreadcrumb(pageId!);
+    const navigate = useNavigate();
+
+    if (loading) return <div>Loading...</div>;
+
+    return (
+        <Container>
+            <BreadcrumbTrail
+                pages={breadcrumb}
+                onNavigate={(url) => {
+                    navigate(url);
+                }}
+            />
+            <p>Hello</p>
+            <p>This is a page: {pageId}</p>
+        </Container>
+    );
 }
