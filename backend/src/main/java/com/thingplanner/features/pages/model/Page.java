06624b8b02cac85ab1fc3b8024d1471507910aa2
OliverKeefe <oliverkeefe@tutanota.com>
2025-09-16 21:20:45 +0100

    feat(backend/pages): add findPage method

--- backend/src/main/java/com/thingplanner/features/pages/model/Page.java
+++ backend/src/main/java/com/thingplanner/features/pages/model/Page.java
@@ -34,4 +34,8 @@ public class Page extends PanacheEntityBase {
         return PanacheEntityBase.find("id", id).firstResult();
     }
 
+    public static Page findPage(String query, Parameters params) {
+        return find(query, params).firstResult();
+    }
+
 }
