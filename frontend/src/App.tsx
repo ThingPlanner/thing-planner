326f1c309e74a78940d5958d3658af5db85afe7f
OliverKeefe <oliverkeefe@tutanota.com>
2025-09-16 21:12:19 +0100

    frontend: add routing to App

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,15 +1,18 @@
 import './App.css'
 import Layout from './components/layout/Layout';
-import CreateEvent from "/home/oliver/Development/ThingPlanner/thing-planner/frontend/src/features/event/components/forms/create-event.tsx";
 import {ThemeProvider} from "@/components/theme/theme-provider.tsx";
+import {BrowserRouter} from "react-router-dom";
+import {AppRoutes} from "@/routes/routes.tsx";
 
 function App() {
 
   return (
     <ThemeProvider defaultTheme="system" storageKey="vite-ui-theme">
-        <Layout>
-            <p>Hello</p>
-        </Layout>
+        <BrowserRouter>
+            <Layout>
+                <AppRoutes />
+            </Layout>
+        </BrowserRouter>
     </ThemeProvider>
   )
 }
