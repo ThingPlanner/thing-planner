cea7179f60c4beddb2ac367df7869dcdfb6a3e2a
OliverKeefe <oliverkeefe@tutanota.com>
2025-09-16 21:13:23 +0100

    frontend(routes): add default route

--- frontend/src/routes/routes.tsx
+++ frontend/src/routes/routes.tsx
@@ -1,9 +1,11 @@
 import { Routes, Route } from "react-router-dom";
+import {Page} from "@/features/shared/pages/page.tsx";
 
 export const AppRoutes = () => {
     return (
         <Routes>
-            <Route path ="/page/:pageId" element{<Page />}
+            <Route path ="/page/:pageId" element={<Page />} />
+            <Route path ="/" element={<Page />} />
         </Routes>
     );
 }
