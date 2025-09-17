092abf5eb73036254d9d6a6b53b7bd2a5b191ede
OliverKeefe <oliverkeefe@tutanota.com>
2025-09-17 19:38:37 +0100

    feat(backend/pages): add UpdatePage usecase

---
+++ backend/src/main/java/com/thingplanner/features/pages/usecase/UpdatePage.java
@@ -0,0 +1,17 @@
+package com.thingplanner.features.pages.usecase;
+
+import jakarta.enterprise.context.ApplicationScoped;
+
+class UpdatePageApi {
+}
+
+
+record UpdatePageRequest () {
+
+}
+
+
+@ApplicationScoped
+class UpdatePageService {
+
+}
