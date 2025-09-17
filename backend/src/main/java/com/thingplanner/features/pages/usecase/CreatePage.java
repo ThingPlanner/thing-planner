f398dca12a1f6d3931d1048dadaf23fc159b688b
OliverKeefe <oliverkeefe@tutanota.com>
2025-09-17 21:18:11 +0100

    feat(backend/pages): add createPage api endpoint method

--- backend/src/main/java/com/thingplanner/features/pages/usecase/CreatePage.java
+++ backend/src/main/java/com/thingplanner/features/pages/usecase/CreatePage.java
@@ -1,12 +1,43 @@
 package com.thingplanner.features.pages.usecase;
 
+import com.thingplanner.features.organization.model.Organization;
+import com.thingplanner.features.pages.model.Page;
+import com.thingplanner.features.things.model.Thing;
 import jakarta.annotation.Resource;
 import jakarta.enterprise.context.ApplicationScoped;
+import jakarta.inject.Inject;
+import jakarta.ws.rs.Consumes;
+import jakarta.ws.rs.POST;
 import jakarta.ws.rs.Path;
+import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.core.MediaType;
+import jakarta.ws.rs.core.Response;
+
+import java.util.UUID;
 
 @Resource
 @Path("/pages")
 class CreatePageApi {
+
+    @Inject CreatePageService createPageService;
+
+    @POST
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Produces(MediaType.APPLICATION_JSON)
+    @Path("/create")
+    public Response createPage(CreatePageRequest request) {
+            boolean created = createPageService.createPage(request);
+
+            if (created) {
+                return Response.status(201)
+                        .entity("New page created successfully.")
+                        .build();
+            } else {
+                return Response.status(424)
+                        .entity("Unable to create new page.")
+                        .build();
+            }
+    }
 }
 
 
