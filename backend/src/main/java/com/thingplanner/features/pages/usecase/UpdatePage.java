a148bd6af59bd65a525f7520feb0f3858b528f19
OliverKeefe <oliverkeefe@tutanota.com>
2025-09-17 21:12:53 +0100

    feat(backend/pages): add updatePage api endpoint

--- backend/src/main/java/com/thingplanner/features/pages/usecase/UpdatePage.java
+++ backend/src/main/java/com/thingplanner/features/pages/usecase/UpdatePage.java
@@ -1,8 +1,48 @@
 package com.thingplanner.features.pages.usecase;
 
-import jakarta.enterprise.context.ApplicationScoped;
 
+import com.thingplanner.features.organization.model.Organization;
+import com.thingplanner.features.pages.model.Page;
+import com.thingplanner.features.things.model.Thing;
+import jakarta.annotation.Resource;
+import jakarta.enterprise.context.ApplicationScoped;
+import jakarta.inject.Inject;
+import jakarta.validation.constraints.NotNull;
+import jakarta.ws.rs.Consumes;
+import jakarta.ws.rs.POST;
+import jakarta.ws.rs.Path;
+import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.core.MediaType;
+import jakarta.ws.rs.core.Response;
+
+import java.util.UUID;
+
+import static io.quarkus.hibernate.orm.panache.PanacheEntityBase.findByIdOptional;
+
+
+@Resource
+@Path("/pages")
 class UpdatePageApi {
+
+    @Inject UpdatePageService updatePageService;
+
+    @POST
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Produces(MediaType.APPLICATION_JSON)
+    @Path("/update")
+    public Response updatePage(UpdatePageRequest request) {
+        boolean isUpdated = updatePageService.updatePage(request);
+        if (!isUpdated) {
+            return Response.status(404)
+                    .entity("Could not update page details.")
+                    .build();
+        }
+
+        return Response.status(200)
+                .entity("Page details updated successfully.")
+                .build();
+    }
+
 }
 
 
