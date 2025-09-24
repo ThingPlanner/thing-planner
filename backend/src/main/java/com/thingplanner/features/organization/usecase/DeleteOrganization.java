38880f5d8adea52062cd234d270158e50a996479
OliverKeefe <oliverkeefe@tutanota.com>
2025-09-24 19:52:30 +0100

    feat(backend/organization): add deleteOrganization api endpoint

--- backend/src/main/java/com/thingplanner/features/organization/usecase/DeleteOrganization.java
+++ backend/src/main/java/com/thingplanner/features/organization/usecase/DeleteOrganization.java
@@ -1,3 +1,38 @@
 package com.thingplanner.features.organization.usecase;
 
+import com.thingplanner.features.calendar.events.model.Event;
+import com.thingplanner.features.organization.model.Organization;
+import jakarta.annotation.Resource;
+import jakarta.enterprise.context.ApplicationScoped;
+import jakarta.inject.Inject;
+import jakarta.ws.rs.*;
+import jakarta.ws.rs.core.MediaType;
+import jakarta.ws.rs.core.Response;
+
+import java.awt.*;
+import java.util.UUID;
+
+@Resource
+@Path("/organization")
+class DeleteOrganizationApi {
+
+    @Inject DeleteOrganizationService service;
+
+    @DELETE
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Produces(MediaType.APPLICATION_JSON)
+    @Path("/organization/delete{id}")
+    public Response deleteOrganization(@PathParam("id") UUID organizationId) {
+        boolean isDeleted = service.delete(organizationId);
+        if (isDeleted) {
+            return Response.status(200)
+                    .entity("Organization deleted successfully")
+                    .build();
+        } else {
+            return Response.status(409)
+                    .entity("Unable to delete organization ID: " + organizationId)
+                    .build();
+        }
+    }
+}
 
