4823ba302d995c39a2703be8355854fcce5a5500
OliverKeefe <oliverkeefe@tutanota.com>
2025-09-24 19:53:26 +0100

    feat(backend/organization): add create service method

--- backend/src/main/java/com/thingplanner/features/organization/usecase/CreateOrganization.java
+++ backend/src/main/java/com/thingplanner/features/organization/usecase/CreateOrganization.java
@@ -1,3 +1,16 @@
 package com.thingplanner.features.organization.usecase;
 
 
+@ApplicationScoped
+class CreateOrganizationService {
+    public boolean create(CreateOrganizationRequest request) {
+        try {
+            Organization organization = new Organization();
+            organization.name = request.name();
+            Organization.persist(organization);
+            return true;
+        } catch (Exception e) {
+            return false;
+        }
+    }
+}
