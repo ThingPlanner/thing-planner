86c579d1bd7458d4fadb20c3190b80a1404c4421
OliverKeefe <oliverkeefe@tutanota.com>
2025-09-24 19:48:33 +0100

    fix(backend/organization): make Organization extend PanacheEntityBase

--- backend/src/main/java/com/thingplanner/features/organization/model/Organization.java
+++ backend/src/main/java/com/thingplanner/features/organization/model/Organization.java
@@ -1,13 +1,15 @@
 package com.thingplanner.features.organization.model;
 
 
+import com.thingplanner.features.calendar.events.model.Event;
+import io.quarkus.hibernate.orm.panache.PanacheEntityBase;
 import jakarta.persistence.*;
 
 import java.util.UUID;
 
 @Entity
 @Table(name = "organization")
-public class Organization {
+public class Organization extends PanacheEntityBase {
 
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     public UUID id;
