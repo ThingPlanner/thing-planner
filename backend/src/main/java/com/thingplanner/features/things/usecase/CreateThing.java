df6eeff68f172edbb2d474fe5fe4afc9e1d91570
OliverKeefe <oliverkeefe@tutanota.com>
2025-09-25 21:02:03 +0100

    fix(backend/thing): add rest annotations

--- backend/src/main/java/com/thingplanner/features/things/usecase/CreateThing.java
+++ backend/src/main/java/com/thingplanner/features/things/usecase/CreateThing.java
@@ -1,10 +1,15 @@
 package com.thingplanner.features.things.usecase;
 
+import com.thingplanner.features.things.model.Thing;
 import com.thingplanner.shared.response.MessageResponse;
 import jakarta.annotation.Resource;
 import jakarta.enterprise.context.ApplicationScoped;
 import jakarta.inject.Inject;
+import jakarta.ws.rs.Consumes;
+import jakarta.ws.rs.POST;
 import jakarta.ws.rs.Path;
+import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.core.MediaType;
 import jakarta.ws.rs.core.Response;
 
 @Resource
@@ -13,6 +18,10 @@ class CreateThingApi {
 
     @Inject CreateThingService service;
 
+    @POST
+    @Path("/create")
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Produces(MediaType.APPLICATION_JSON)
     public Response createThing(CreateThingRequest request) {
         boolean isCreated = service.create(request);
 
