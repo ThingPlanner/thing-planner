e9c53eeedbfb219fa9acab081ba89c16a2e74a04
OliverKeefe <oliverkeefe@tutanota.com>
2025-09-25 21:02:57 +0100

    feat(backend/thing): add getThingById api endpoint

--- backend/src/main/java/com/thingplanner/features/things/usecase/GetThing.java
+++ backend/src/main/java/com/thingplanner/features/things/usecase/GetThing.java
@@ -1,13 +1,39 @@
 package com.thingplanner.features.things.usecase;
 
 
+import com.thingplanner.shared.response.MessageResponse;
 import jakarta.annotation.Resource;
 import jakarta.enterprise.context.ApplicationScoped;
-import jakarta.ws.rs.Path;
+import jakarta.inject.Inject;
+import jakarta.ws.rs.*;
+import jakarta.ws.rs.core.MediaType;
+import jakarta.ws.rs.core.Response;
+
+import java.util.UUID;
 
 @Resource
 @Path("/thing")
-class GetThingApi {}
+class GetThingApi {
+
+    @Inject GetThingService service;
+
+    @GET
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Produces(MediaType.APPLICATION_JSON)
+    @Path("/get{id}")
+    public Response getThingById(@PathParam("id") UUID id) {
+        GetThingResponse response = service.getById(id);
+        if (response != null) {
+            return Response.status(200)
+                    .entity(response)
+                    .build();
+        } else {
+            return Response.status(404)
+                    .entity(new MessageResponse("Failure", "Could not find Thing with ID: " + id))
+                    .build();
+        }
+    }
+}
 
 record GetThingRequest () {}
 
