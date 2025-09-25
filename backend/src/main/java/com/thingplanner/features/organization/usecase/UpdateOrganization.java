90fadd5bc8bdcf6ce411ef5c2c57a05e1eab10a8
OliverKeefe <oliverkeefe@tutanota.com>
2025-09-25 21:04:20 +0100

    feat(backend/organization): add UpdateOrganization api endpoint

--- backend/src/main/java/com/thingplanner/features/organization/usecase/UpdateOrganization.java
+++ backend/src/main/java/com/thingplanner/features/organization/usecase/UpdateOrganization.java
@@ -1,3 +1,40 @@
 package com.thingplanner.features.organization.usecase;
 
 
+import com.thingplanner.features.organization.model.Organization;
+import jakarta.annotation.Resource;
+import jakarta.enterprise.context.ApplicationScoped;
+import jakarta.inject.Inject;
+import jakarta.validation.constraints.NotNull;
+import jakarta.ws.rs.Consumes;
+import jakarta.ws.rs.Path;
+import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.core.MediaType;
+import jakarta.ws.rs.core.Response;
+
+import java.util.UUID;
+
+@Resource
+@Path("/organization")
+class UpdateOrganizationApi {
+
+    @Inject UpdateOrganizationService service;
+
+    @Path("/update")
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response updateOrganization(UpdateOrganizationRequest request) {
+        boolean isUpdated = service.update(request);
+
+        if (isUpdated) {
+            return Response.status(200)
+                    .entity("Organization updated successfully.")
+                    .build();
+        } else {
+            return Response.status(409)
+                    .entity("Could not update organization.")
+                    .build();
+        }
+    }
+}
+
