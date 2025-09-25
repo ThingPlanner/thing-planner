53fa0c3befa25f751ffa76f7e1d90dda38e5a42f
OliverKeefe <oliverkeefe@tutanota.com>
2025-09-25 18:14:48 +0100

    feat(backend/thing): add delete service method

--- backend/src/main/java/com/thingplanner/features/things/usecase/DeleteThing.java
+++ backend/src/main/java/com/thingplanner/features/things/usecase/DeleteThing.java
@@ -1,5 +1,6 @@
 package com.thingplanner.features.things.usecase;
 
+import com.thingplanner.features.things.model.Thing;
 import com.thingplanner.shared.response.MessageResponse;
 import jakarta.annotation.Resource;
 import jakarta.enterprise.context.ApplicationScoped;
@@ -30,14 +31,25 @@ class DeleteThingApi {
                     .build();
         } else {
             return Response.status(404)
-                    .entity(new MessageResponse("Failure", "Unable to delete Thing with ID: " + id));
+                    .entity(new MessageResponse("Failure", "Unable to delete Thing with ID: " + id))
+                    .build();
         }
     }
 }
 
-record DeleteThingRequest () {}
-
-record DeleteThingResponse () {}
-
 @ApplicationScoped
-class DeleteThingService {}
+class DeleteThingService {
+
+    public boolean delete(UUID id) {
+        if (exists(id)) {
+            Thing.deleteById(id);
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    private boolean exists(UUID id) {
+        return Thing.count("id", id) > 0;
+    }
+}
