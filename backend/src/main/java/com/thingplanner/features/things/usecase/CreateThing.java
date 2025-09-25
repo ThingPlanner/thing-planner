43b63e49ee5b40e814cb9e5a78b62733c48f2afb
OliverKeefe <oliverkeefe@tutanota.com>
2025-09-25 11:44:02 +0100

    feat(backend/thing): add createThing api endpoint

--- backend/src/main/java/com/thingplanner/features/things/usecase/CreateThing.java
+++ backend/src/main/java/com/thingplanner/features/things/usecase/CreateThing.java
@@ -9,7 +9,24 @@ import jakarta.ws.rs.core.Response;
 
 @Resource
 @Path("/thing")
-class CreateThingApi {}
+class CreateThingApi {
+
+    @Inject CreateThingService service;
+
+    public Response createThing(CreateThingRequest request) {
+        boolean isCreated = service.create(request);
+
+        if (isCreated) {
+            return Response.status(200)
+                    .entity(new MessageResponse("Success", "New Thing created."))
+                    .build();
+        } else {
+            return Response.status(409)
+                    .entity(new MessageResponse("Failure", "Could not create Thing."))
+                    .build();
+        }
+    }
+}
 
 record CreateThingRequest () {}
 
