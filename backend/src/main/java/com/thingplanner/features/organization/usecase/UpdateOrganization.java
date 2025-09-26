29c384b720715397bd9e38c96efa294b9ec41ba3
OliverKeefe <oliverkeefe@tutanota.com>
2025-09-26 14:57:06 +0100

    fix(backend/organization): add POST annotation

--- backend/src/main/java/com/thingplanner/features/organization/usecase/UpdateOrganization.java
+++ backend/src/main/java/com/thingplanner/features/organization/usecase/UpdateOrganization.java
@@ -7,6 +7,7 @@ import jakarta.enterprise.context.ApplicationScoped;
 import jakarta.inject.Inject;
 import jakarta.validation.constraints.NotNull;
 import jakarta.ws.rs.Consumes;
+import jakarta.ws.rs.POST;
 import jakarta.ws.rs.Path;
 import jakarta.ws.rs.Produces;
 import jakarta.ws.rs.core.MediaType;
@@ -20,6 +21,7 @@ class UpdateOrganizationApi {
 
     @Inject UpdateOrganizationService service;
 
+    @POST
     @Path("/update")
     @Consumes(MediaType.APPLICATION_JSON)
     @Produces(MediaType.APPLICATION_JSON)
