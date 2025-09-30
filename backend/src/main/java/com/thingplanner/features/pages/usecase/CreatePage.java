1be8e3ac3f0002c3cc6ebfd1d392687a95161961
OliverKeefe <oliverkeefe@tutanota.com>
2025-09-30 21:55:08 +0100

    refactor(backend): re-write create service method

--- backend/src/main/java/com/thingplanner/features/pages/usecase/CreatePage.java
+++ backend/src/main/java/com/thingplanner/features/pages/usecase/CreatePage.java
@@ -3,6 +3,7 @@ package com.thingplanner.features.pages.usecase;
 import com.thingplanner.features.organization.model.Organization;
 import com.thingplanner.features.pages.model.Page;
 import com.thingplanner.features.things.model.Thing;
+import io.quarkus.logging.Log;
 import jakarta.annotation.Resource;
 import jakarta.enterprise.context.ApplicationScoped;
 import jakarta.inject.Inject;
@@ -60,17 +61,34 @@ class CreatePageService {
     @Transactional
     public boolean create(CreatePageRequest request) {
         Page page = new Page();
-        page.id = UUID.randomUUID();
         page.title = request.title();
-        page.organization = request.organization();
-        page.thing = request.thing();
-        page.parent = request.parent();
-        page.url = "/" + request.organization().id + "/" + request.thing().id + "/" + page.id;
+        Organization organization = Organization.findById(request.organizationId());
+        if (organization.isPersistent()) {
+            page.organization = organization;
+        }
+
+        Thing thing = Thing.findById(request.thingId());
+        if (thing.isPersistent()) {
+            page.thing = thing;
+        }
+
+        Page parentPage = null;
+
+        if (request.parentId() != null) {
+            try {
+                parentPage = Page.findById(request.parentId());
+            } catch (NotFoundException e) {
+                Log.error("Could not find parent page with ID: " + request.parentId() + ", setting parentId to null.");
+            }
+        }
+
+        page.parent = parentPage;
 
         try {
-            Page.persist(page);
+            page.persistAndFlush();
             return true;
         } catch (Exception e) {
+            Log.error("Unable to persist new page: " + page.id);
             return false;
         }
     }
