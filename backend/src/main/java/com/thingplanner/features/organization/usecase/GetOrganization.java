9041f2a99fcaafdd5fbb20a89b0c3358d230881a
OliverKeefe <oliverkeefe@tutanota.com>
2025-09-30 21:52:08 +0100

    feat(backend): add get service method

--- backend/src/main/java/com/thingplanner/features/organization/usecase/GetOrganization.java
+++ backend/src/main/java/com/thingplanner/features/organization/usecase/GetOrganization.java
@@ -1,14 +1,18 @@
 package com.thingplanner.features.organization.usecase;
 
+import com.fasterxml.jackson.annotation.JsonAutoDetect;
 import com.thingplanner.features.organization.model.Organization;
 import com.thingplanner.shared.response.MessageResponse;
+import io.quarkus.panache.common.Parameters;
 import jakarta.annotation.Resource;
 import jakarta.enterprise.context.ApplicationScoped;
 import jakarta.inject.Inject;
+import jakarta.inject.Singleton;
 import jakarta.ws.rs.*;
 import jakarta.ws.rs.core.MediaType;
 import jakarta.ws.rs.core.Response;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.UUID;
 
@@ -74,3 +78,33 @@ class GetOrganizationService {
                 ))
                 .orElse(null);
     }
+
+    public List<GetOrganizationResponse> get(GetOrganizationRequest request) {
+
+        StringBuilder query = new StringBuilder("FROM Organization o WHERE 1=1");
+        Parameters params = null;
+
+        if (request.id() != null) {
+            query.append(" AND o.id = :id");
+            params = addParameter(params, "id", request.id());
+        }
+
+        if (request.name() != null) {
+            query.append(" AND o.name = :name");
+            params = addParameter(params, "name", request.name());
+        }
+
+        List<Organization> organizations = Organization.findAllOrganizations(query.toString(), params);
+
+        return organizations.stream()
+                .map(o -> new GetOrganizationResponse(
+                        o.id,
+                        o.name
+                ))
+                .toList();
+    }
+
+    private Parameters addParameter(Parameters params, String name, Object value) {
+        return params == null ? Parameters.with(name, value) : params.and(name, value);
+    }
+}
